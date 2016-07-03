c774a420bf587b7f15710a431849d385320e2760
Kevek <kevin.r.kerr@gmail.com>
2016-07-03 02:48:07 -0400

    Rewrite spawn code Start thinking about preloading some state to pass   around to creeps and the spawn

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 var creepHarvester = require('creep.harvester');
 var creepUpgrader = require('creep.upgrader');
 var creepBuilder = require('creep.builder');
-// Room
-var roomSpawner = require('room.spawner');
+// Spawn
+require('prototype.spawn')();
 // Construction
 var roomConstruction = require('room.construction');
+// State
+var gameState=require('game.state');
 module.exports.loop = function() {
     var harvesterCount = 0;
     var upgraderCount = 0;
@@ -21,8 +23,30 @@ module.exports.loop = function() {
             }
         }
         room.memory.knownControllerLevel=room.controller.level;
-    }
 
+        // TODO: Does this allocate a new room state? Is this the right way to do this???
+        var roomState={
+        	numHarvesters: 0,
+        	numUpgraders: 0,
+        	numBuilders: 0,
+        	energyStores: []
+        };
+        // var structures=room.find(FIND_MY_STRUCTURES);
+        // // TODO: Do I need to get the name and parse???
+        // for (let structure in structures) {
+        //     // TODO: Account for other types of structures...
+        //     if (structure.structureType === STRUCTURE_EXTENSION ||
+        //         structure.structureType === STRUCTURE_SPAWN ||
+        //         structure.structureType === STRUCTURE_STORAGE) {
+        //         roomState.energyStores.push({
+        //             id: structure.id,
+        //             availableCapacity: structure.energyCapacity - structure.energy
+        //         })
+        //     }
+        // }
+        gameState.rooms[name]=roomState;
+    }
+    
     // Run each creep
     for (let name in Memory.creeps) {
         // If a creep has expired, delete it from memory
@@ -33,20 +57,22 @@ module.exports.loop = function() {
         }
         if (creep.memory.role === 'harvester') {
             creepHarvester.run(creep);
+            gameState.rooms[creep.room.name].numHarvesters+=1;
             harvesterCount++;
         }
         if (creep.memory.role === 'upgrader') {
             creepUpgrader.run(creep);
-            upgraderCount++;
+            gameState.rooms[creep.room.name].numUpgraders+=1;
         }
         if (creep.memory.role === 'builder') {
             creepBuilder.run(creep);
-            builderCount++;
+            gameState.rooms[creep.room.name].numBuilders+=1;
         }
     }
-    roomSpawner.spawn({
-        harvesterCount: harvesterCount,
-        upgraderCount: upgraderCount,
-        builderCount: builderCount
-    });
+    
+    console.log(JSON.stringify(gameState));
+    console.log(harvesterCount);
+
+// TODO: Do this for all spawns
+    Game.spawns.Spawn1.considerSpawning(gameState);
 }
---
+++ src/prototype.spawn.js
@@ -0,0 +1,73 @@
+module.exports = function() {
+	StructureSpawn.prototype.considerSpawning = function(gameState) {
+		var roomState=gameState.rooms[this.room.name];
+		if(roomState===undefined) {
+			Game.notify("massive error: expected room " + this.room.name + " in game state");
+			return;
+		}
+
+		var targetHarvesters = 5;
+		// Require a minimum for 300 energy to spawn a creep, 
+		// Be at least 90% full before bothing to spawn a creep except if we need harvesters
+		if (this.room.energyAvailable < 300 || 
+			(roomState.numHarvesters >= targetHarvesters && 
+			 this.room.energyAvailable / this.room.energyCapacityAvailable < 0.9)) {
+			return;
+		} 
+		
+		console.log("HERE");
+		
+	    var targetUpgraders = 1;
+	    var targetBuilders = 4;
+	    var bodyPartToEnergyRequired={
+	    	"work": 100,
+	    	"carry": 50,
+	    	"move": 50,
+	    }
+
+	    var role=undefined;
+	    var bodyParts=[];
+	    var remainingEnergyAvailable=this.room.energyAvailable;
+	    var iter=0;
+	    var roleSequence=[];
+	    if (roomState.numHarvesters < targetHarvesters) {
+	    	role = "harvester";
+	    	roleSequence=[WORK, WORK, CARRY, MOVE];
+	    } else if (roomState.numUpgraders < targetUpgraders) {
+	    	role = "upgrader";
+	    	roleSequence=[WORK, CARRY, MOVE, MOVE];
+	    } else if (roomState.numBuilders < targetBuilders) {
+	    	role = "builder";
+	    	roleSequence=[WORK, WORK, CARRY, MOVE];
+	    }
+
+	    // Calculate the largest body we can create
+	    // TODO: Be smarter about these sequences -- e.g., we may not actually want
+	    // TODO: x/4 move parts for a harvester? This is a simplistic start.
+	    if (roleSequence.length>0) {
+		    var nextPartEnergy=bodyPartToEnergyRequired[roleSequence[iter%roleSequence.length]];
+	    	while(remainingEnergyAvailable>=nextPartEnergy && iter < 10) {
+	    	    console.log("remainingEnergyAvailable: "+remainingEnergyAvailable);
+	    	    console.log("nextPartEnergy: "+nextPartEnergy);
+	    	    
+	    		bodyParts.push(roleSequence[iter%roleSequence.length]);
+	    		remainingEnergyAvailable-=nextPartEnergy;
+	    		iter++;
+	    		
+	    		console.log("iter%roleSequence.length: "+iter%roleSequence.length);
+	    		console.log("roleSequence[iter%roleSequence.length]: " + roleSequence[iter%roleSequence.length]);
+	    		nextPartEnergy=bodyPartToEnergyRequired[roleSequence[iter%roleSequence.length]];
+	    		console.log("nextPartEnergy: "+nextPartEnergy);
+	    	}
+
+            console.log(JSON.stringify(bodyParts));
+
+	    	if (role!=undefined && bodyParts.length > 0) {
+    	    	this.createCreep(bodyParts, undefined, {
+    	            role: role,
+    	            working: false
+    	        });
+	        }
+    	}
+	}
+}
